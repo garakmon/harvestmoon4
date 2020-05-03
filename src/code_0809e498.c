0f2040461b90be0b391f78119a721b947a2b812c
alonsolop255 <alonsolop255@gmail.com>
2020-05-02 21:16:08 -0500

    Refactored bachelorette ram struct

--- src/code_0809e498.c
+++ src/code_0809e498.c
@@ -5,14 +5,17 @@ typedef unsigned int u32;
 typedef int s32;
 
 struct BACHELORETTE_RAM_DATA {
-    u32 unknown0; //0-3
-    u32 unknown1; //4-7
-    u32 unknown2; //8-11
-    u32 unknown3; //12-15
-    u32 unknown4; //16-19 
-    u16 Affection; //20-21
-    u8 unknown5; //22
-    u8 unknown6; //23
+    u32 unknown0; //0-3    0x0-3
+    u32 unknown1; //4-7    0x4-7
+    u32 unknown2; //8-11   0x8-B
+    u32 unknown3; //12-15  0xC-F
+    u32 unknown4; //16-19  0x10-13
+    u16 Affection; //20-21 0x14-15
+    u32 f22:3; //
+    u32 f23:3; //
+    u32 f24:3; //
+    u32 f25:3; //
+    u32 f26:3; //
 };
 
 //returns a bachelorette's Affection
@@ -20,25 +23,22 @@ u16 sub_809E498(struct BACHELORETTE_RAM_DATA *t){
     return t->Affection;
 }
 
-//Not sure what these functions do, my guess is related to flags
+//These return the 3 bit unknown values
+//sub_809E4B4 is a duplicate of sub_809E4A4
 u32 sub_809E49C(struct BACHELORETTE_RAM_DATA *t){
-    return ((u32)t->unknown5 << 0x1d) >> 0x1d;
+    return t->f22;
 }
-
 u32 sub_809E4A4(struct BACHELORETTE_RAM_DATA *t){
-    return ((u32)t->unknown6 << 0x1c) >> 0x1d;
+    return t->f25;
 }
-
-u32 sub_809E4AC(u32 r0){
-    return ((u32)*(u16 *)(r0 + 0x16) << 0x17) >> 0x1d;
+u32 sub_809E4AC(struct BACHELORETTE_RAM_DATA *t){
+    return t->f24;
 }
-
 u32 sub_809E4B4(struct BACHELORETTE_RAM_DATA *t){
-    return ((u32)t->unknown6 << 0x1c) >> 0x1d;
+    return t->f25;
 }
-
 u32 sub_809E4BC(struct BACHELORETTE_RAM_DATA *t){
-    return ((u32)t->unknown6 << 0x19) >> 0x1d;
+    return t->f26;
 }
 
 //Increases a bachelorette's affection

83ec355f5f0351ee784950f2a2c761fb4814536f
alonsolop255 <alonsolop255@gmail.com>
2020-05-02 19:45:46 -0500

    Refactored NPC ram struct and functions

--- src/code_0809e33c.c
+++ src/code_0809e33c.c
@@ -6,8 +6,18 @@ struct NPC_RAM_DATA {
     u32 unknown0; //0-3
     u32 unknown1; //4-7
     u8 Affection; //8
-    u8 Flags1; //9
-    u8 Flags2; //10
+    u32 f10:5; //
+    u32 f11:1; // F_SPOKEN_TODAY
+    u32 f12:1; // F_SPOKEN_CURRENT_AREA
+    u32 f13:1; // F_GIVEN_GIFT
+    u32 f14:1; // F_SPOKEN_EVER
+    u32 f15:1; // 
+    u32 f16:1; // 
+    u32 f17:1; // 
+    u32 f18:1; // 
+    u32 f19:1; // 
+    u32 f1a:1; // 
+    u32 f1b:1; //
 };
 
 //returns an NPC's Affection
@@ -15,27 +25,23 @@ u8 sub_809E33C(struct NPC_RAM_DATA *t) {
     return t->Affection;
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////
-//All the following functions return the specified flag in bit 0 with all other bits cleared//
-//////////////////////////////////////////////////////////////////////////////////////////////
-
-//Returns an unknown flag 
+//Returns an unknown 5 bit value
 u8 sub_809E340(struct NPC_RAM_DATA *t) {
-    return ((u32)t->Flags1 << 0x1b) >> 0x1b;
+    return t->f10;
 }
 //Returns F_GIVEN_GIFT
 u8 sub_809E348(struct NPC_RAM_DATA *t) {
-    return t->Flags1 >> 7;
+    return t->f13;
 }
 //Returns F_SPOKEN_TODAY
 u8 sub_809E350(struct NPC_RAM_DATA *t) {
-    return ((u32)t->Flags1 << 0x1a) >> 0x1f;
+    return t->f11;
 }
 //Returns F_SPOKEN_CURRENT_AREA
 u8 sub_809E358(struct NPC_RAM_DATA *t) {
-    return ((u32)t->Flags1 << 0x19) >> 0x1f;
+    return t->f12;
 }
 //Returns F_SPOKEN_EVER
 u8 sub_809E360(struct NPC_RAM_DATA *t) {
-    return ((u32)t->Flags2 << 0x1f) >> 0x1f;
+    return t->f14;
 }
